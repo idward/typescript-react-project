ccea0b2d9f10a6ff90323a7248a908a9ea7b3cb9
idward <idward@vip.sina.com>
2019-08-25 08:30:14 +0800

    update files

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,16 +3,16 @@ import Button from 'antd/lib/button';
 import 'antd/lib/button/style/css';
 
 interface IAppProps {
-    message: string
+  message: string;
 }
 
 const App = ({ message }: IAppProps) => {
-    return (
-        <div className="container">
-            <h1>Hello {message}</h1>
-            <Button type="primary">Test</Button>
-        </div>
-    );
+  return (
+    <div className="container">
+      <h1>Hello {message}</h1>
+      <Button type="primary">Test</Button>
+    </div>
+  );
 };
 
 export default App;
